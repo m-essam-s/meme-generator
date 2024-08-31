ec6edaab1b1def8d3b3cad8f537f7af7f548e2ac
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-31 20:04:19 +0300

    init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <header>
+            <img className="logo" src="/icons/troll-face.png" alt="troll-face.png" />
+            <h1>Meme Generator</h1>
+            <h3>React Course - Project 3</h3>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Meme.jsx
@@ -0,0 +1,33 @@
+import memesData from "./memesData";
+
+const Meme = () => {
+
+    function getMemeImage() {
+        console.log("Clicked")
+    }
+    
+    return (
+        <main>
+            <form className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme;
